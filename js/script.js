8f4069b61baea52c78e47f22d4907fb5f90ead47
Tommy <tommy@gmail.com>
2023-03-06 11:42:13 -0800

    Defined constant array and courses

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let course1 = {
+    code: "ACIT 1630",
+    name: "Database Systems"
+};
+
+let course2 = {
+    code: "ACIT 1620",
+    name: "Fundamental Web Technologies"
+};
+
+let course3 = {
+    code: "ACIT 1515",
+    name: "Scripting for IT"
+};
+
+const courseList = [course1, course2,  course3];
